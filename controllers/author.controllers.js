66cb8481e0c0b393fbcf99340dec426d95f0027b
zahera <zahera.shehab@protonmail.com>
2026-07-18 21:36:09 +0300

    Add GET authors details

--- controllers/author.controllers.js
+++ controllers/author.controllers.js
@@ -8,6 +8,14 @@ router.get("/", async (req, res) => {
     res.render("author/all-authors.ejs", { authors: allAuthors });
 });
 
+router.get("/:id", async (req, res) => {
+    console.log(req.params.id)
+    const foundAuthor = await Author.findById(req.params.id)
+    res.render("author/author-details.eejs", { author: foundAuthor })
+});
+
+
+
 
 
 module.exports = router;
